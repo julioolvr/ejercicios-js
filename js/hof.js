82f76ea13bd8b9e6ab696bffcc35cc2734bcd6a6
Julio Olivera <julio.olvr@gmail.com>
2016-12-21 18:17:10 -0300

    Third question solved

--- js/hof.js
+++ js/hof.js
@@ -1,1 +1,14 @@
-module.exports = () => {}
+module.exports = fn => {
+  const results = {}
+
+  return x => {
+    if (results[x] !== undefined) {
+      return results[x]
+    }
+
+    const result = fn(x)
+    results[x] = result
+
+    return result
+  }
+}
